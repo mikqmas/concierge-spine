cebd76a41feebe7eba2e0a3d55ecf0725c8b6d30
samiyokim <samiyokim@gmail.com>
2025-01-27 20:30:17 -0800

    add testimonials and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Landing from './compontents/Landing'
+import Testimonials from './compontents/Testimonials'
+import Footer from './compontents/Footer'
 
 function App() {
 
   return (
     <>
       <Landing />
-      
+      <Testimonials />
+      <Footer />
     </>
   )
 }
